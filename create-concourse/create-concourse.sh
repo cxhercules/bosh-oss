f3b71a162671703572eac8e6a7f48110cf606661
cxhercules <cxhercules@users.noreply.github.com>
2018-08-21 10:15:37 -0700

    Added credhub, and bosh stemcell upload script for latest ubunty-trusty

--- create-concourse/create-concourse.sh
+++ create-concourse/create-concourse.sh
@@ -1,21 +1,31 @@
-#Deploy concourse to BOSH OSS
+#!/bin/bash
 
-bosh deploy -d concourse concourse-bosh-deployment/cluster/concourse.yml \
-  -l concourse-bosh-deployment/versions.yml \
+cd $(dirname $0)
+
+## Checkout the particular concourse version, in this particular case a branch that has what I need
+if [ ! -d concourse-bosh-deployment ]; then
+   git clone -b pr-uaa-credhub https://github.com/cxhercules/concourse-bosh-deployment.git
+else
+   pushd concourse-bosh-deployment
+      ## getting any new changes I may have added to branch, and checking out the branch just in case
+      git pull
+      git checkout pr-uaa-credhub
+   popd
+fi
+
+
+#Deploy concourse to BOSH OSS
+bosh -n deploy -d concourse concourse-bosh-deployment/cluster/concourse.yml \
   -o concourse-bosh-deployment/cluster/operations/static-web.yml \
+  -o concourse-bosh-deployment/cluster/operations/tls-vars.yml \
   -o concourse-bosh-deployment/cluster/operations/tls.yml \
-  -o create-concourse/ops-files/tls-vars.yml \
   -o concourse-bosh-deployment/cluster/operations/basic-auth.yml \
+  -o concourse-bosh-deployment/cluster/operations/basic-auth-vars.yml \
   -o concourse-bosh-deployment/cluster/operations/privileged-https.yml \
   -o concourse-bosh-deployment/cluster/operations/scale.yml \
-  --var web_ip=10.0.0.12 \
-  --var external_url=https://35.200.172.165 \
-  --var network_name=default \
-  --var web_vm_type=default \
-  --var db_vm_type=large \
-  --var db_persistent_disk_type=large \
-  --var worker_vm_type=large \
-  --var deployment_name=concourse \
-  --var web_instances=1 \
-  --var worker_instances=1 \
-  --var concourse_domain_name=35.200.172.165
+  -o concourse-bosh-deployment/cluster/operations/postgres-link.yml \
+  -o concourse-bosh-deployment/cluster/operations/credhub-colocated.yml \
+  -o concourse-bosh-deployment/cluster/operations/uaa.yml \
+  -o concourse-bosh-deployment/cluster/operations/azs.yml \
+  -l concourse-bosh-deployment/versions.yml \
+  -l vars.yml
