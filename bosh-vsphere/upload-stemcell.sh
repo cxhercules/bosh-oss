71892456bd0d3c7768734c3eb4fc62716e824015
cxhercules <cxhercules@users.noreply.github.com>
2018-08-31 10:15:44 -0700

    Kubo setup, not fully tested

--- bosh-vsphere/upload-stemcell.sh
+++ bosh-vsphere/upload-stemcell.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Ubuntu trusty
 bosh_cmd=$(curl -s https://bosh.cloudfoundry.org/stemcells/bosh-vsphere-esxi-ubuntu-trusty-go_agent|grep -A2 "bosh upload-stemcell"|sed -e 's/<div.*<pre>//g' -e 's/<a.*">//g' -e 's/<\/a>.*$//g' -e 's/\\//g' |tr -d '\n')
+
+# Ubuntu Xenial
+bosh_cmd=$(curl -s https://bosh.io/stemcells/bosh-vsphere-esxi-ubuntu-xenial-go_agent|grep -A2 "bosh upload-stemcell"|sed -e 's/<div.*<pre>//g' -e 's/<a.*">//g' -e 's/<\/a>.*$//g' -e 's/\\//g' |tr -d '\n')
 
 eval $bosh_cmd
